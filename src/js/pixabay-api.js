cb90c4de983e9f66a327060d75e6ec7530e6675b
marharyta zozulia <zozulia.marharyta@gmail.com>
2025-07-31 20:41:38 +0200

    build

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '51408648-c1ffe0deedea697cddb1d0199';
 
-export async function getImagesByQuery(query, page = 1) {
+export async function getImagesByQuery(query, page = 1, perPage = 15) {
   const params = {
     key: API_KEY,
     q: query,
--- src/main.js
+++ src/main.js
@@ -1,42 +1,61 @@
 import './css/styles.css';
 import { getImagesByQuery } from './js/pixabay-api';
-import { createGallery, clearGallery, showLoader, hideLoader } from './js/render-functions';
+import { createGallery, clearGallery, showLoader, hideLoader,showLoadMoreButton,
+    hideLoadMoreButton } from './js/render-functions';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.form');
 const input = document.querySelector('input[name="search-text"]');
+const loadMoreBtn = document.querySelector('.load-more');
+
+
+let query = '';
+let page = 1;
+const perPage = 15;
+let totalHits = 0;
 
 form.addEventListener('submit', async e => {
   e.preventDefault();
 
-  const query = input.value.trim();
-  if (!query) {
-    iziToast.warning({ message: 'Please enter a search query.' });
-    return;
-  }
+  query = form.elements['search-text'].value.trim();
+    if (!query) return;
 
-  clearGallery();
-  showLoader();
+    page = 1;
+    clearGallery();
+    hideLoadMoreButton();
+    showLoader();
 
-  try {
-    const data = await getImagesByQuery(query);
-    const { hits } = data;
 
-    if (hits.length === 0) {
-      iziToast.info({
-        message: 'Sorry, there are no images matching your search query. Please try again!',
-      });
-    } else {
-      createGallery(hits);
+    try {
+        const { hits, totalHits: total } = await getImagesByQuery(query, page);
+        hideLoader();
+        if (hits.length === 0) {
+            iziToast.warning({
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+                position: 'topRight',
+            });
+            return;
+        }
+        totalHits = total;
+        createGallery(hits);
+        showLoadMoreButton();
+        const totalPages = Math.ceil(totalHits / perPage);
+        if (page >= totalPages) {
+            hideLoadMoreButton();
+            iziToast.info({
+            message: "The end of search results.",
+            position: 'topRight',
+  });
+}
+    } catch (error) {   
+        hideLoader();
+        iziToast.error({
+      message: 'We are sorry, but you have reached the end of search results.',
+      position: 'topRight',
+    });
     }
-  } catch (error) {
-    iziToast.error({ message: 'Something went wrong. Please try again later.' });
-  } finally {
-    hideLoader();
-  }
 });
-
 
 
 loadMoreBtn.addEventListener('click', async () => {
